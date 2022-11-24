809f56e0b183f3f5e5dc3e16f4f80ca44bbee674
rogelio.medina <rogelio.medina@rogelio-ltm39w6.internal.salesforce.com>
2022-11-24 03:51:24 -0600

    login

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ app.get('/', (req, res) => {
 
 });
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'login.html'))
+});
+
 //Lanzar la app
 app.listen(PORT,()=>{
     console.log("servidor en línea");
