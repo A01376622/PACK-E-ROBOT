6bafc8dc42fb9314d544234bee9ba2b24bb4f8d7
Michelle Sánchez <A01376622@itesm.mx>
2022-11-24 00:34:09 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// importar las bibliotecas
+const { Console } = require('console');
+const express = require('express');
+const path = require('path');
+// const consoleRoutes = require('./routes/console')
+
+
+const app = express(); // create an express application
+const PORT = 8080
+
+//middleware
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/examen',consoleRoutes);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'landing_page.html'))
+});
+
+//Lanzar la app
+app.listen(PORT,()=>{
+    console.log("servidor en línea");
+});
